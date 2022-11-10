c9262aa49901594a5ae8e92a347a67128fabbb79
bigrabbit555 <sdell624@gmail.com>
2022-11-10 14:09:02 -0500

    cart?

--- src/App.js
+++ src/App.js
@@ -13,19 +13,37 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
   const [cart, setCart] = useState({});
-  const [totalPrice, setTotalPrice] = useState(0);
+  setCart = (newItem, cost) => {
+    // Increment if the item is already in the cart
+    if (newItem in cart) {
+      cart[newItem] += 1;
+    // Otherwise add item to cart at quantity 1
+    } else {
+      cart[newItem] = 1;
+    }
+    // Add the cost of the new item to the total cost
+    incrementTotalPrice(cost)
+  }
+  const [totalPrice, incrementTotalPrice] = useState(0);
+  incrementTotalPrice = (addition) => {
+    totalPrice += addition;
+  }
 
   return (
     <div className="App">
       <h1>Spenny D's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <BakerItem i={item}/>
+        <BakerItem i={item} addToCart={setCart}/>
       ))}
 
       <div>
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        {cart.map((elt) => {  
+          <p>{elt.value}x {elt.key}</p>
+        })}
+        <p>Total Price: ${totalPrice}</p>
       </div>
     </div>
   );
